1fe995e991e3acd771262e64184ec3a9ee2f659e
Anton Voychuk Ranchuk <anton@voychuk.com>
2024-06-23 22:37:23 +0200

    Devutils added

---
+++ installers/devutils.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# VScode on Ubuntu
+
+# Check if the script is being executed as root
+if [ "$(id -u)" -ne 0 ]; then
+	echo " > This script must be executed as root. Run the script again with 'sudo $0'."
+	exit 1
+fi
+
+# Update the package list
+echo " > Updating package list..."
+apt update
+
+# Install git and github
+echo " > Installing git and gh..."
+apt install -y git gh
+
+# Install node and npm
+echo " > Installing nodejs and npm..."
+apt install -y nodejs npm
+# Install node version manager
+npm install -g n
