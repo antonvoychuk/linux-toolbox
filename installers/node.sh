01da5452d13b5c4509a770badf90c8c5338a412f
Anton <anton@voychuk.com>
2024-06-26 06:34:41 +0200

    sudo check shortened

--- installers/node.sh
+++ installers/node.sh
@@ -3,10 +3,7 @@
 # NodeJS on Ubuntu
 
 # Check if the script is being executed as root
-if [ "$(id -u)" -ne 0 ]; then
-	echo " > This script must be executed as root. Run the script again with 'sudo $0'."
-	exit 1
-fi
+if [ "$(id -u)" -ne 0 ]; then echo " > Run the script again with 'sudo $0'."; exit 1; fi
 
 # Update the package list
 echo " > Updating package list..."
