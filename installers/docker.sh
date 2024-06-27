f04e592cbc12595b548e9b09ca06bf922b6e823a
Anton <anton@voychuk.com>
2024-06-27 18:13:17 +0200

    Docker installation wrapper added

---
+++ installers/docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if the script is being executed as root
+if [ "$(id -u)" -ne 0 ]; then echo " > Run the script again with 'sudo $0'."; exit 1; fi
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+chmod +x get-docker.sh
+sh ./get-docker.sh --dry-run
+rm -f get-docker.sh
+usermod -aG docker $USER
